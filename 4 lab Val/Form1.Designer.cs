684c7bba399b7cb1b676f4b0f4eb45cdf083bae2
Matttokin <55289178+Matttokin@users.noreply.github.com>
2020-05-12 11:31:47 +0300

    Initial commit

---
+++ 4 lab Val/Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace _4_lab_Val
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(168, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Введите 16-и символьный ключ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(168, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "1234567890123456";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 51);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(165, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Применить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(269, 23);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(183, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Выбрать";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(266, 7);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(186, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Выберите файл для записи данных";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(266, 74);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(179, 13);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Введите данные для встраивания";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(269, 90);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(183, 20);
+            this.textBox4.TabIndex = 1;
+            this.textBox4.Text = "f";
+            // 
+            // button4
+            // 
+            this.button4.Enabled = false;
+            this.button4.Location = new System.Drawing.Point(269, 116);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(183, 23);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "Применить";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(491, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(176, 13);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Введите файл для чтения данных";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(491, 90);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(202, 20);
+            this.textBox5.TabIndex = 1;
+            this.textBox5.Text = "1";
+            // 
+            // button5
+            // 
+            this.button5.Enabled = false;
+            this.button5.Location = new System.Drawing.Point(491, 25);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(202, 23);
+            this.button5.TabIndex = 2;
+            this.button5.Text = "Выбрать";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Enabled = false;
+            this.button6.Location = new System.Drawing.Point(491, 116);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(202, 23);
+            this.button6.TabIndex = 2;
+            this.button6.Text = "Применить";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(494, 74);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(199, 13);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "Введите длинну считываемых данных";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(491, 51);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.ReadOnly = true;
+            this.textBox6.Size = new System.Drawing.Size(202, 20);
+            this.textBox6.TabIndex = 1;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(269, 51);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.ReadOnly = true;
+            this.textBox7.Size = new System.Drawing.Size(183, 20);
+            this.textBox7.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ 4 lab Val/Form1.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _4_lab_Val
+{
+    public partial class Form1 : Form
+    {
+        static String folderFileWrite;
+        static String folderFileRead;
+        static int keyMod = 1; // требуется для определения позиции пикселя по ключу
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        static int getColor(int bite, int R, int G, int B) //расчет цвета пикселя на основе бита
+        {
+            //расчитываем по формулам https://habr.com/ru/post/115287/
+            double lam = 0.2;
+            if (bite == 1)
+            {
+                return (int)(B + lam * (0.3 * R + 0.59 * G + 0.11 * B));
+            }
+            else
+            {
+                return (int)(B - lam * (0.3 * R + 0.59 * G + 0.11 * B));
+            }
+        }
+
+        static int getBite(Bitmap image, int x, int y) //рассчет бита на основе цвета пикселя
+        {
+
+            int meanBlue;
+            int sumBlue = 0;
+
+            
+                sumBlue += image.GetPixel(x + 1, y).B;
+                sumBlue += image.GetPixel(x - 1, y).B;
+                sumBlue += image.GetPixel(x, y + 1).B;
+                sumBlue += image.GetPixel(x, y - 1).B;
+
+            meanBlue = sumBlue / 4;
+
+            if (image.GetPixel(x, y).B > meanBlue)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        static int[,] string2Array(String stringIn) //получение массива байт в виде чисел
+        {
+            int[,] arr = new int[stringIn.Length, 8];
+
+            for (int i = 0; i < stringIn.Length; i++)
+            {
+                String str = Convert.ToString(stringIn[i], 2).PadLeft(8, '0');
+                for (int j = 0; j < 8; j++)
+                {
+                    arr[i, j] = Convert.ToInt32(str.Substring(j, 1));
+                }
+            }
+            return arr;
+        }
+        int[] getArray(int[,] dataArr,int num) //получение подмассива
+        {
+            int[] arr = new int[8];
+            for (int i = 0; i < 8; i++)
+            {
+                arr[i] = dataArr[num, i];
+            }
+            return arr;
+        }
+
+        private void button2_Click(object sender, EventArgs e) //выбор файл для записи
+        {
+            try
+            {
+                if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+                    return;
+                // получаем выбранный файл
+                folderFileWrite = openFileDialog1.FileName;
+                textBox7.Text = folderFileWrite;
+                button4.Enabled = true;
+            } catch
+            {
+                MessageBox.Show("Произошла ошибка");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e) //встраивание данных
+        {
+            try
+            {
+                string stringIn = textBox4.Text.ToString();
+                Bitmap image = new Bitmap(folderFileWrite, true);
+
+                int[,] dataArr = string2Array(stringIn);
+
+                for (int j = 0; j < dataArr.Length / 8; j++)
+                {
+                    int[] arr = getArray(dataArr, j);
+
+                    for (int i = 0; i < arr.Length; i++)
+                    {
+                        int[] arrayPixel = calcPos(j * 8 + i, image.Width); 
+                        Color palet = image.GetPixel(arrayPixel[0], arrayPixel[1]);
+                        Color bcolor = Color.FromArgb(palet.R, palet.G, getColor(arr[i], palet.R, palet.G, palet.B)); 
+                        image.SetPixel(arrayPixel[0], arrayPixel[1], bcolor); 
+                    }
+                }
+
+                MessageBox.Show("Выберите файл для сохранения");
+                if (saveFileDialog1.ShowDialog() == DialogResult.Cancel)
+                    return;
+                image.Save(saveFileDialog1.FileName);
+                image.Dispose();
+
+                MessageBox.Show("Данные записаны");
+            }
+            catch
+            {
+                MessageBox.Show("Произошла ошибка при записи");
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e) //чтение встроенных данных
+        {
+            try
+            {
+                int lenght = Convert.ToInt32(textBox5.Text.ToString());
+                Bitmap image = new Bitmap(@folderFileRead, true);
+                String stringOut = "";
+                for (int j = 0; j < lenght; j++)
+                {
+                    int[] arr = new int[8]; 
+                    for (int i = 0; i < arr.Length; i++)
+                    {
+                        int[] arrayPixel = calcPos(j * 8 + i, image.Width); 
+                        arr[i] = getBite(image, arrayPixel[0], arrayPixel[1]); 
+                    }
+                    stringOut += (char)Convert.ToInt32(array2String(arr), 2);
+                }
+
+                MessageBox.Show(stringOut.ToString(), "Считанные данные");
+                image.Dispose();
+            } catch
+            {
+                MessageBox.Show("Произошла ошибка при чтении");
+            }
+        }
+        string array2String(int[] arr)
+        {
+            String str = "";
+            for (int i = 0; i < arr.Length; i++)
+            {
+                str += arr[i].ToString();
+            }
+            return str;
+        }
+        private void button1_Click(object sender, EventArgs e) //ввод ключа
+        {
+            if(textBox1.Text.Length == 16)
+            {
+                string tmpStr  = textBox1.Text.ToString();
+
+                int valKey = 0;
+                for(int i= 0; i < 16; i++)
+                {
+                    valKey += (int)tmpStr[i];
+                }
+                keyMod = valKey % 2;
+
+                button2.Enabled = true;
+                button5.Enabled = true;
+                MessageBox.Show("Ключ сохранен");
+            } else
+            {
+                MessageBox.Show("Неверная длина!");
+            }
+        }
+        int[] calcPos(int num, int weight) //генератор позиций пикселей
+        {
+            weight -= 10;
+
+            int tmpWeight = num * 6;
+            int tmpHeight = 0;
+            if(keyMod == 1)
+            {
+                tmpWeight += 1;
+                tmpHeight += 2;
+            }
+            if (tmpWeight > weight)
+            {
+                tmpHeight = tmpWeight / weight;
+                tmpWeight =  (tmpWeight - tmpHeight * weight) / 6;
+
+                return new int[2] { tmpWeight * 6 + 5, tmpHeight * 6 + 5 };
+            } else
+            {
+                return new int[2] { tmpWeight+ 5, tmpHeight + 5 };
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e) //выбор файла для чтения
+        {
+            try { 
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            // получаем выбранный файл
+            folderFileRead = openFileDialog1.FileName;
+            textBox6.Text = folderFileRead;
+            button6.Enabled = true;
+            }
+            catch
+            {
+                MessageBox.Show("Произошла ошибка");
+            }
+        }
+    }
+}
+
